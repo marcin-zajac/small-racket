295bf7285dabedb1e3ea82ace81685b028fcf7f9
marcin-zajac <57039654+marcin-zajac@users.noreply.github.com>
2020-11-14 14:07:14 +0100

    handle private routes

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Switch, Route, Redirect } from 'react-router-dom';
 import Rocket from '../atoms/Rocket';
 import AuthPage from '../pages/AuthPage';
 import MainContent from '../pages/MainContent';
+import PrivateRoute from './PrivateRoute';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/auth" component={AuthPage} />
       <Route exact path="/auth" component={Rocket} />
-      <Route path="/user" component={MainContent} />
+      <PrivateRoute path="/user" component={MainContent} />
       <Route exact path="/">
         <Redirect to="/user" /> 
       </Route>
